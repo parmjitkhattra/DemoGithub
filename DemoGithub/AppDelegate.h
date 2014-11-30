dd4c7be045fc4bbc3151193cbb32bcf54fca7a9c
hello <parmjit.khattra@gmail.com>
2014-11-30 23:04:01 +0530

    Initial Commit

---
+++ DemoGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoGithub
+//
+//  Created by KHATTRA on 30/11/14.
+//  Copyright (c) 2014 xenon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGithub
+//
+//  Created by KHATTRA on 30/11/14.
+//  Copyright (c) 2014 xenon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
